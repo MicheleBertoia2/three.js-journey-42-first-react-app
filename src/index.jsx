d3fff26cf9756e172ed5e56793e377a11f280a56
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-09-03 18:10:19 +0200

    [basic usage]

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,19 @@ import { createRoot } from 'react-dom/client'
 
 const root = createRoot(document.querySelector('#root'))
 
-const toto = 'tata'
+const toto = 'there'
 
 root.render(
     <>
-        <h1 className='main-title'>Hello {toto}</h1>
-        <p>Lorem ipsum dolor sit <br /> amet consectetur adipisicing elit. Doloremque, cupiditate?</p>
+        <h1 
+            style={
+                {
+                    color: 'coral',
+                    backgroundColor: 'floralwhite'
+                }
+            }
+            >Hello {toto}</h1>
+        <p className='cute-paragraph'>Lorem ipsum dolor sit <br /> amet consectetur adipisicing elit. Doloremque, cupiditate?</p>
         <input type="checkbox" id='check'/>
         <label htmlFor="check">that checkbox</label>
 
