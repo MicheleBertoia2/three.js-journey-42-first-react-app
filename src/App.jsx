a08559ba31f31e330f1baf418e1bddec054d3a02
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-09-03 18:21:25 +0200

    [components]

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App()
+{
+
+  console.log('js is still working');
+
+  return <h1>My App</h1>
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,29 @@
 import './style.css'
 import { createRoot } from 'react-dom/client'
 
+import App from './App.jsx'
+
 const root = createRoot(document.querySelector('#root'))
 
 const toto = 'there'
 
 root.render(
-    <>
-        <h1 
-            style={
-                {
-                    color: 'coral',
-                    backgroundColor: 'floralwhite'
-                }
-            }
-            >Hello {toto}</h1>
-        <p className='cute-paragraph'>Lorem ipsum dolor sit <br /> amet consectetur adipisicing elit. Doloremque, cupiditate?</p>
-        <input type="checkbox" id='check'/>
-        <label htmlFor="check">that checkbox</label>
+    // <>
+    //     <h1 
+    //         style={
+    //             {
+    //                 color: 'coral',
+    //                 backgroundColor: 'floralwhite'
+    //             }
+    //         }
+    //         >Hello {toto}</h1>
+    //     <p className='cute-paragraph'>Lorem ipsum dolor sit <br /> amet consectetur adipisicing elit. Doloremque, cupiditate?</p>
+    //     <input type="checkbox" id='check'/>
+    //     <label htmlFor="check">that checkbox</label>
 
-    </>
+    // </>
+
+    <div>
+        <App />
+    </div>
 )
